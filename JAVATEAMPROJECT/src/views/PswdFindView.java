10265520e07f4315fcb921555cb2901d3b8fdd11
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2023-11-25 16:29:34 +0900

    Fix : 뷰 수정
    PswdFindView에 number pad 추가

--- JAVATEAMPROJECT/src/views/PswdFindView.java
+++ JAVATEAMPROJECT/src/views/PswdFindView.java
@@ -1,68 +1,73 @@
 package views;
 
 import custom_component.DefaultFont;
+import custom_component.NumberPadPanel;
+import sun.java2d.loops.CustomComponent;
 
 import javax.swing.*;
 import java.awt.*;
 
 public class PswdFindView extends JPanel {
-    JLabel phonelb,birthlb;
-    JTextField phonetf,birthtf;
-    JButton inquirybt;
-    public PswdFindView(){
+    private static final int FONT_SIZE = 80;
+    JLabel phonelb, birthlb; // lb는 label을 의미
+    JTextField phonetf, birthtf; // tf는 textfield를 의미
+    JButton inquirybt; // bt는 button을 의미
+    NumberPadPanel np;
+
+    public PswdFindView() {
         this.setLayout(new BorderLayout());
-        this.setBackground(Color.red);
+        // BorderLayout의 Center
+        JPanel blc = new JPanel(new GridBagLayout());
+        add(blc, BorderLayout.CENTER);
 
-        //BorderLayout의 Center
-        JPanel ctp = new JPanel();
-        ctp.setLayout(new FlowLayout());
-        ctp.setBackground(Color.yellow);
+            //Grid Bag Layout의 left
+        GridBagConstraints gtf = new GridBagConstraints();
+        gtf.weightx = 2;
+        JPanel glc = new JPanel(new GridLayout(4,1));
 
-        //ctp JPanel의 top 부분
-        JPanel ctptop = new JPanel();
-        ctptop.setLayout(new FlowLayout());
-        ctptop.setBackground(Color.pink);
+            //center-1
+        glc.add(new JPanel());
+            //center-2
+        JPanel center2 = new JPanel();
+        phonelb = new JLabel("연락처 : ");
+        phonelb.setFont(new DefaultFont(FONT_SIZE));
+        center2.add(phonelb);
+        phonetf = new JTextField(10);
+        phonetf.setFont(new DefaultFont(FONT_SIZE));
+        center2.add(phonetf);
+        glc.add(center2);
+            //center-3
+        JPanel center3 = new JPanel();
+        birthlb = new JLabel("생년월일 : ");
+        birthlb.setFont(new DefaultFont(FONT_SIZE));
+        center3.add(birthlb);
+        birthtf = new JTextField(10);
+        birthtf.setFont(new DefaultFont(FONT_SIZE));
+        center3.add(birthtf);
+        glc.add(center3);
+            //center-4
+        glc.add(new JPanel());
+        blc.add(glc,gtf);
 
-        phonelb = new JLabel("연락처 :");
-        phonetf = new JTextField(8);
-        phonelb.setFont(new DefaultFont(80));
-        phonetf.setFont(new DefaultFont(80));
-        phonetf.setBorder(BorderFactory.createEmptyBorder(0,0,0,100));
+            //Grid Bag Layout의 right
+        JPanel gbr = new JPanel();
+        GridBagConstraints gnp = new GridBagConstraints();
+        gnp.weightx = 1;
 
-        ctptop.add(phonelb);
-        ctptop.add(phonetf);
+        np = new NumberPadPanel();
+        gbr.add(np);
+        blc.add(gbr,gnp);
 
-        //ctp JPanel의 bottom 부분
-        JPanel ctpbtm = new JPanel();
-        ctpbtm.setLayout(new FlowLayout());
-        ctpbtm.setBackground(Color.blue);
-
-        birthlb = new JLabel("생년월일 :");
-        birthtf = new JTextField(8);
-        birthlb.setFont(new DefaultFont(80));
-        birthtf.setFont(new DefaultFont(80));
-        birthtf.setBorder(BorderFactory.createEmptyBorder(0,0,0,40));
-
-        ctpbtm.add(birthlb);
-        ctpbtm.add(birthtf);
-
-        ctp.add(ctptop);
-        ctp.add(ctpbtm);
-
-        add(ctp,BorderLayout.CENTER);
-
-        //BorderLayout의 South
+        // BorderLayout의 South
         JPanel stp = new JPanel();
+        add(stp, BorderLayout.SOUTH);
         stp.setLayout(new FlowLayout());
         stp.setBackground(Color.gray);
 
         inquirybt = new JButton("조회");
-        inquirybt.setPreferredSize(new Dimension(200,100));
-        inquirybt.setFont(new DefaultFont(80));
+        inquirybt.setPreferredSize(new Dimension(200, 100));
+        inquirybt.setFont(new DefaultFont(FONT_SIZE));
         stp.add(inquirybt);
-
-        add(stp,BorderLayout.SOUTH);
     }
-
 
 }

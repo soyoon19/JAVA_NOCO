5ce8847f029491c14ddcc760910420c4d618a68c
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2023-11-25 12:07:57 +0900

    Feat : 뷰 생성
    ManagerLoginView 관리자 로그인 페이지 생성

--- JAVATEAMPROJECT/src/Main.java
+++ JAVATEAMPROJECT/src/Main.java
@@ -1,5 +1,5 @@
 import views.DefaultFrame;
-import views.PswdFindView;
+import views.ManagerLoginView;
 
 import java.awt.*;
 import javax.swing.*;
@@ -8,7 +8,7 @@ public class Main {
     public static void main(String[] args) {
         DefaultFrame win = new DefaultFrame();
         win.setSize(1920,1080);
-        win.add(new PswdFindView());
+        win.add(new ManagerLoginView());
         win.setVisible(true);
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }

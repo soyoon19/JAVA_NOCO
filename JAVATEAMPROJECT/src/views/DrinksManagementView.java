80af2c5744937c423b66045ec2402934f52d302b
SDE <songdongeun00@gmail.com>
2023-11-27 06:16:32 +0900

    feat : 음료 관리 메인 페이지 (우측 패널 생성) JTable 사용

--- JAVATEAMPROJECT/src/views/DrinksManagementView.java
+++ JAVATEAMPROJECT/src/views/DrinksManagementView.java
@@ -6,6 +6,7 @@ import java.awt.*;
 public class DrinksManagementView extends JPanel{
     JPanel drinksCategoryPanel, drinksDetailPanel;
 
+
     //left: drinksCategoryPanel | Right: drinksDetailPanel
     public DrinksManagementView(){
         this.setLayout(new GridBagLayout());
@@ -14,7 +15,7 @@ public class DrinksManagementView extends JPanel{
         drinksCategoryPanel = new JPanel();
         drinksCategoryPanel.setLayout(new BorderLayout());
         this.add(drinksCategoryPanel, DefaultFrame.easyGridBagConstraint(0, 0, 2, 1));
-        drinksDetailPanel.setBackground(Color.red);
+        drinksCategoryPanel.setBackground(Color.red);
 
 
         //right : drinksDetailPanel
@@ -31,15 +32,32 @@ public class DrinksManagementView extends JPanel{
         //top : 음료명 검색
         JPanel top = new JPanel();
 
+
         //center : 카테고리
         JPanel center = new JPanel();
+
 
         //bottom : 추가/편집/삭제/일괄설정 버튼 넣기
         JPanel bottom = new JPanel();
     }
 
     class drinksDetailPanel extends  JPanel {
-        //상품 정보 출력 (JTable 이용)
+        //상품 정보 출력 (JTable 이용)\
+        String[] columnType = {"음료코드", "음료명", "분류", "상태", "판매 가능 개수", "이벤트 여부", "판매가", "단가"};
+        Object[][] drinksData = {
+                //테스트를 위한 데이터
+                {"CF01", "아메리카노", "커피", "판매", 20, "X", 3000, 800},
+                {"CF01", "아메리카노", "커피", "판매", 20, "X", 3000, 800},
+                {"CF01", "아메리카노", "커피", "판매", 20, "X", 3000, 800},
+                {"CF01", "아메리카노", "커피", "판매", 20, "X", 3000, 800},
+                {"CF01", "아메리카노", "커피", "판매", 20, "X", 3000, 800},
+        };
+
+        JTable table = new JTable(drinksData, columnType);
+        JScrollPane scrollPane = new JScrollPane(table);
+
+
+
     }
 
 }

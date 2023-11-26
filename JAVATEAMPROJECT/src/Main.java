6f2796248a8b4691da1ae0f43d1593aadda173d4
soyoon19 <96063941+soyoon19@users.noreply.github.com>
2023-11-27 04:55:53 +0900

    Feat : 메인 같이 올려요
    ManagerLoginView 수정2

--- JAVATEAMPROJECT/src/Main.java
+++ JAVATEAMPROJECT/src/Main.java
@@ -1,15 +1,13 @@
-import views.DefaultFrame;
-import views.PswdFindView;
+import views.*;
 
 import java.awt.*;
 import javax.swing.*;
-
 
 public class Main {
     public static void main(String[] args) {
         DefaultFrame win = new DefaultFrame();
         win.setSize(1920,1080);
-        win.add(new PswdFindView());
+        win.add(new ManagerMainView());
         win.setVisible(true);
         win.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
